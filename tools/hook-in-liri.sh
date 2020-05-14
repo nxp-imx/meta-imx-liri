4735a44bea1c13d94a11ee28919e30cbcbdddcff
Neena Busireddy <neenareddy.busireddy@nxp.com>
2020-05-13 22:04:57 -0500

    hook-in-liri.sh: Add meta-mortsgna to bblayers
    Signed-off-by: Neena Busireddy <neenareddy.busireddy@nxp.com>

--- tools/hook-in-liri.sh
+++ tools/hook-in-liri.sh
@@ -5,6 +5,7 @@ echo "# Hook in Liri Desktop layers " >> conf/bblayers.conf
 echo "BBLAYERS += \" \${BSPDIR}/sources/meta-liri \"" >> conf/bblayers.conf
 echo "BBLAYERS += \" \${BSPDIR}/sources/meta-qt5-extra \"" >> conf/bblayers.conf
 echo "BBLAYERS += \" \${BSPDIR}/sources/meta-imx-liri \"" >> conf/bblayers.conf
+echo "BBLAYERS += \" \${BSPDIR}/sources/meta-mortsgna \"" >> conf/bblayers.conf
 echo >> conf/bblayers.conf
 
 echo "Liri Desktop layers added to bblayers.conf"
